168ce352a786ed19fdaa134f1f007e89cd7b1e16
Your Name <you@example.com>
2026-02-25 11:08:05 +0000

    caches

--- modules/flake/nixos-classes/nixos.nix
+++ modules/flake/nixos-classes/nixos.nix
@@ -1,19 +1,30 @@
 { inputs, ... }:
 {
   flake.modules.nixos.nixos = {
-    imports = [ inputs.home-manager.nixosModules.home-manager ];
+    imports = [
+      inputs.home-manager.nixosModules.home-manager
+      inputs.hyprland.nixosModules.default
+    ];
+    nix.settings = {
+      substituters = [
+        "https://cache.nixos.org"
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+    };
     nix.settings.experimental-features = [
       "nix-command"
       "flakes"
     ];
     nixpkgs.config.allowUnfree = true;
+    nixpkgs.config.permittedInsecurePackages = [ "openssl-1.1.1w" ];
     users.users.chek = {
       isNormalUser = true;
       home = "/home/chek";
       extraGroups = [ "wheel" ];
     };
-    nixpkgs.config.permittedInsecurePackages = [
-      "openssl-1.1.1w"
-    ];
   };
 }
