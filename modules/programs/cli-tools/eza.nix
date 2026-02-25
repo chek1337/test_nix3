a8026eccf4b52cee30b09050674ffbfc74837d39
Your Name <you@example.com>
2026-02-25 13:32:20 +0000

    eza aliases

--- modules/programs/cli-tools/eza.nix
+++ modules/programs/cli-tools/eza.nix
@@ -1,12 +1,18 @@
 {
   flake.modules.homeManager.eza =
     { pkgs, ... }:
+    let
+      aliases = {
+        l = "eza --grid --color=always --no-filesize --no-time --no-user --no-permissions --icons";
+        ls = "eza --color=always --long --git --icons";
+        lsa = "eza --color=always --long --git --icons -a";
+        tree = "eza --color=always --long --git --icons --tree";
+      };
+    in
     {
       home.packages = with pkgs; [ eza ];
-      programs.zsh.shellAliases = {
-        ls = "eza";
-        ll = "eza -l";
-        la = "eza -la";
-      };
+
+      programs.zsh.shellAliases = aliases;
+      programs.nushell.shellAliases = aliases;
     };
 }
