9e6dbea3845ba2fec6dcd74c2bf1dee0f29aee9c
chek1337 <DaniPlay1337@yandex.ru>
2026-08-15 21:39:36 +0700

    feat: fix moonlight vulkan

--- modules/services/remote-access/moonlight.nix
+++ modules/services/remote-access/moonlight.nix
@@ -4,10 +4,14 @@
     {
       lib,
       config,
-      pkgs,
+      pkgs-stable,
       ...
     }:
     lib.mkIf config.settings.enableMoonlightClient {
-      environment.systemPackages = [ pkgs.moonlight-qt ];
+      # Пин moonlight-qt на stable: в unstable ffmpeg обновился до 9.0, где из
+      # AVVulkanDeviceContext убрали queue_family_*_index / nb_*_queues, а
+      # moonlight 6.1.0 всё ещё пишет в эти поля (plvk.cpp) и не компилируется.
+      # Вернуть на pkgs.moonlight-qt, когда апстрим починит Vulkan-рендерер.
+      environment.systemPackages = [ pkgs-stable.moonlight-qt ];
     };
 }
